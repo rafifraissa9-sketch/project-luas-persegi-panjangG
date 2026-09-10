0dd99f5ff089ff4c8e8d28c874601b3b78ebb9fb
Rafif Raissa Yunus <rafifraissa9@gmail.com>
2026-09-10 15:08:38 +0700

    Project luas persegi panjang

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+public class helloworld {
+
+    public class void main(string[]args) {
+    {
+        int bilangan=1;
+        //tempat ekspresi logika)
+        while(bilangan<=10)
+        {
+            bilangan++;//1
+        }
+        System.out.println("Akhir dari perulangan : "+bilangan);
+    
+}
